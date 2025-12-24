2fef2ce8b501c9aa3974a0178df121747df9c620
caakdeniz <ca.akdeniz@gmail.com>
2025-12-24 12:41:32 -0600

    FeedCast Alpha: 10-day forecast with look-ahead logic and calendar UI

---
+++ advisor/views.py
@@ -0,0 +1,63 @@
+import requests 
+from django.shortcuts import render
+from datetime import datetime
+
+def home(request):
+    # Get location from the URL (or use Boulder,CO as default)
+    lat = request.GET.get('lat', '40.0150')
+    lon = request.GET.get('lon', '-105.2705')
+
+    # Build the weather API request URL
+    api_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&daily=temperature_2m_max,precipitation_probability_max&timezone=auto&forecast_days=10&temperature_unit=fahrenheit"
+
+    response = requests.get(api_url).json()
+    daily_data = response.get('daily', {})
+
+    forecast_list = []
+
+    for i in range(len(daily_data.get('time', []))):
+        temp = daily_data['temperature_2m_max'][i]
+        rain = daily_data['precipitation_probability_max'][i]
+        
+        # Get tomorrow's rain if it exists
+        tomorrow_rain = None
+        if i + 1 < len(daily_data['time']):
+            tomorrow_rain = daily_data['precipitation_probability_max'][i+1]
+
+        status, message = get_lawn_advice(temp, rain, tomorrow_rain)
+
+        date_obj = datetime.strptime(daily_data['time'][i], '%Y-%m-%d')
+        forecast_list.append({
+            'day_name': date_obj.strftime('%A'),
+            'day_num': date_obj.strftime('%d'),
+            'month': date_obj.strftime('%b'),
+            'temp': temp,
+            'rain': rain,
+            'status': status,
+            'msg': message
+        })
+
+    context = {'forecast': forecast_list, 'lat': lat, 'lon': lon}
+    return render(request, 'advisor/index.html', context)
+
+def get_lawn_advice(temp, rain, next_day_rain=None):
+    # Base Logic
+    if rain > 50:
+        status = "RED"
+        message = "STAY OFF: Heavy rain predicted. Nutrients will wash away."
+    elif temp > 90:
+        status = "RED"
+        message = "STAY OFF: Extreme heat. Grass is stressed; fertilizer may burn it."
+    elif 60 <= temp <= 85 and rain < 20:
+        status = "GREEN"
+        message = "IDEAL: Perfect temperature and low rain for application."
+    else:
+        status = "YELLOW"
+        message = "CAUTION: Conditions are okay, but not perfect. Check local wind."
+
+    # Look-ahead Logic
+    if status == "GREEN" and next_day_rain is not None and next_day_rain > 70:
+        status = "YELLOW"
+        message = "CAUTION: Good today, but heavy rain tomorrow could cause runoff."
+
+    return status, message
